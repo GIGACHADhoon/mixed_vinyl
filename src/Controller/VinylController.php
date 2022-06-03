45d28564eb9d109cdc02bacf7f265486be2df3f2
GIGACHADhoon <keyboardwarriorhoon@gmail.com>
2022-06-03 16:54:45 +1000

    before adding another symphony package

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -2,15 +2,22 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use function Symfony\Component\String\u;
 
-class VinylController
+class VinylController extends AbstractController
 {
     #[Route('/')]
     public function homepage(){
-        return new Response('Title: PB and Jams');
+
+        $tracks = [
+            ['song'=>'Quiver','artist'=>'Darren Styles'],
+            ['song'=>'Famous','artist'=>'21 Savage'],
+        ];
+
+        return $this->render('homepage.html.twig',['title'=>'PB & Jams','tracks'=>$tracks,]);
     }
 
     #[Route('/browse/{genre}')]
